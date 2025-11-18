17bae3567f3bd704b3e59c278e6557370393fc6d
Mario Nizic <marionizic@msn.com>
2025-11-18 19:09:48 +0100

    Added new methods to ui_tasks.py, implemented ui_tasks.py methods in test_smoke.py instead of POM methods

--- tasks/ui_tasks.py
+++ tasks/ui_tasks.py
@@ -1,40 +1,57 @@
 from playwright.sync_api import Page
 
-def navigate_to(page: Page, url: str, timeout: int = 10_000) -> None:
-    page.goto(url, timeout=timeout, wait_until="networkidle")
+def navigate_to_product_page(page: Page):
+    page.goto("/")
+    page.get_by_role("heading", name="Thor Hammer").click()
 
-def click_when_visible(page: Page, selector: str, timeout: int = 10_000) -> None:
-    page.wait_for_selector(selector, state="visible", timeout=timeout)
-    page.click(selector, timeout=timeout)
+def add_product_to_cart(page: Page):
+    page.get_by_role("button", name="Add to cart").click()
+    page.locator("[data-test='nav-cart']").click()
 
-def type_input(page: Page, selector: str, value: str, timeout: int = 10_000) -> None:
-    page.wait_for_selector(selector, state="visible", timeout=timeout)
-    page.fill(selector, value)
+def navigate_to_registration(page: Page):
+    page.get_by_role("button", name="Proceed to checkout").click()
+    page.get_by_role("link", name="Register your account").click()
 
-def select_option(page: Page, selector: str, value: str, timeout: int = 10_000) -> None:
-    page.wait_for_selector(selector, state="visible", timeout=timeout)
-    page.select_option(selector, value)
+def complete_registration(page:Page, first_name, last_name, date_of_birth, street, postal_code, city, state, country, phone, email, password):
+    page.get_by_label("First name").fill(first_name)
+    page.get_by_label("Last name").fill(last_name)
+    page.get_by_label("Date of birth").fill(date_of_birth)
+    page.get_by_label("Street").fill(street)
+    page.get_by_label("Postal code").fill(postal_code)
+    page.get_by_label("City").fill(city)
+    page.get_by_label("State").fill(state)
+    page.get_by_label("Country").select_option(country)
+    page.get_by_label("Phone").fill(phone)
+    page.get_by_label("Email").fill(email)
+    page.get_by_label("Password").fill(password)
+    page.get_by_role("button", name="Register").click()
 
-def complete_registration(page:Page, first_name_selector:str, last_name_selector:str, date_of_birth_selector:str, street_selector:str, postal_code_selector:str, city_selector:str, state_selector:str, country_selector:str, phone_selector:str, email_selector:str, password_selector:str, first_name:str, last_name:str, date_of_birth:str, street:str, postal_code:str, city:str, state:str, country:str, phone:str, email:str, password:str):
-    type_input(page, first_name_selector, first_name)
-    type_input(page, last_name_selector, last_name)
-    type_input(page, date_of_birth_selector, date_of_birth)
-    type_input(page, street_selector, street)
-    type_input(page, postal_code_selector, postal_code)
-    type_input(page, city_selector, city)
-    type_input(page, state_selector, state)
-    select_option(page, country_selector, country)
-    type_input(page, phone_selector, phone)
-    type_input(page, email_selector, email)
-    type_input(page, password_selector, password)
+def complete_login(page:Page, email, password):
+    page.get_by_label("Email address *").fill(email)
+    page.get_by_label("Password *").fill(password)
+    page.locator("[data-test='login-submit']").click()
 
-def complete_login(page:Page, email_selector:str, password_selector:str, email:str, password:str):
-    type_input(page, email_selector, email)
-    type_input(page, password_selector, password)
+def navigate_to_cart_login(page: Page):
+    page.goto("/checkout")
+    page.get_by_role("button", name="Proceed to checkout").click()
 
-def select_payment_method(page:Page, payment_method_selector:str, credit_card_number_selector:str, expiration_date_selector:str, cvv_selector:str, card_holder_name_selector:str, payment_method:str, credit_card_number:str, expiration_date:str, cvv:str, card_holder_name:str):
-    select_option(page, payment_method_selector, payment_method)
-    type_input(page, credit_card_number_selector, credit_card_number)
-    type_input(page, expiration_date_selector, expiration_date)
-    type_input(page, cvv_selector, cvv)
-    type_input(page, card_holder_name_selector, card_holder_name)
+def complete_cart_login(page: Page, email, password):
+    page.locator("#email").fill(email)
+    page.locator("#password").fill(password)
+    page.locator("[data-test='login-submit']").click()
+
+def proceed_to_payment(page: Page):
+    page.get_by_role("button", name="Proceed to checkout").click()
+    page.get_by_role("button", name="Proceed to checkout").click()
+
+def complete_payment(page: Page, payment_method, credit_card_number, expiration_date, cvv, credit_card_holder_name):
+    page.get_by_label("Payment Method").select_option(payment_method)
+    page.get_by_label("Credit Card Number").fill(credit_card_number)
+    page.get_by_label("Expiration Date").fill(expiration_date)
+    page.get_by_label("CVV").fill(cvv)
+    page.get_by_label("Card Holder Name").fill(credit_card_holder_name)
+    page.get_by_role("button", name="Confirm").click()
+
+def check_invoice(page: Page):
+    page.locator("#menu").click()
+    page.locator("[data-test='nav-my-invoices']").click()
--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -1,6 +1,10 @@
 import pytest
 import allure
+from playwright.sync_api import Page
 
+from tasks.ui_tasks import navigate_to_product_page, add_product_to_cart, navigate_to_registration, \
+    complete_registration, complete_login, navigate_to_cart_login, complete_cart_login, proceed_to_payment, \
+    complete_payment, check_invoice
 from utilities.user_data import USERS
 
 @allure.title("Smoke Test Demo Application")
@@ -12,66 +16,24 @@ from utilities.user_data import USERS
 @allure.suite("Smoke")
 @allure.testcase("DWA-250")
 @pytest.mark.smoke
-def test_smoke(homepage, product_page, cart_page, register_page, login_page, payment_page):
+def test_smoke(page: Page):
 
-        homepage.navigate()
+    navigate_to_product_page(page)
 
-        homepage.click_on_product_title()
+    add_product_to_cart(page)
 
-        product_page.click_on_add_to_cart_btn()
+    navigate_to_registration(page)
 
-        product_page.click_on_add_to_cart_icon()
+    complete_registration(page,USERS.user_credentials.first_name,USERS.user_credentials.last_name,USERS.user_credentials.date_of_birth,USERS.user_credentials.street, USERS.user_credentials.postal_code, USERS.user_credentials.city, USERS.user_credentials.state, USERS.user_credentials.country, USERS.user_credentials.phone, USERS.user_credentials.email , USERS.user_credentials.password)
 
-        cart_page.click_on_proceed_to_checkout_btn()
+    complete_login(page,USERS.user_credentials.email,USERS.user_credentials.password)
 
-        cart_page.click_on_register_account_link()
+    navigate_to_cart_login(page)
 
-        register_page.enter_first_name(USERS.user_credentials.first_name)
+    complete_cart_login(page, USERS.user_credentials.email, USERS.user_credentials.password)
 
-        register_page.enter_last_name(USERS.user_credentials.last_name)
+    proceed_to_payment(page)
 
-        register_page.enter_date_of_birth(USERS.user_credentials.date_of_birth)
+    complete_payment(page, USERS.user_credentials.payment_method, USERS.user_credentials.credit_card_number, USERS.user_credentials.expiration_date, USERS.user_credentials.cvv, USERS.user_credentials.card_holder_name)
 
-        register_page.enter_street(USERS.user_credentials.street)
-
-        register_page.enter_postal_code(USERS.user_credentials.postal_code)
-
-        register_page.enter_city(USERS.user_credentials.city)
-
-        register_page.enter_state(USERS.user_credentials.state)
-
-        register_page.select_country(USERS.user_credentials.country)
-
-        register_page.enter_phone(USERS.user_credentials.phone)
-
-        register_page.enter_email(USERS.user_credentials.email)
-
-        register_page.enter_password(USERS.user_credentials.password)
-
-        register_page.click_on_register_btn()
-
-        login_page.login_user(USERS.user_credentials.email, USERS.user_credentials.password)
-
-        login_page.click_on_login_btn()
-
-        cart_page.navigate_to_cart()
-
-        cart_page.click_on_proceed_to_checkout_btn()
-
-        cart_page.enter_user_email(USERS.user_credentials.email)
-
-        cart_page.enter_user_password(USERS.user_credentials.password)
-
-        cart_page.click_on_login_btn_cart()
-
-        cart_page.click_on_proceed_to_checkout_btn()
-
-        cart_page.click_on_proceed_to_checkout_btn()
-
-        payment_page.select_payment_method(USERS.user_credentials.payment_method, USERS.user_credentials.credit_card_number, USERS.user_credentials.expiration_date, USERS.user_credentials.cvv, USERS.user_credentials.card_holder_name)
-
-        payment_page.click_on_confirm_btn()
-
-        payment_page.click_on_username()
-
-        payment_page.click_on_my_invoices()
+    check_invoice(page)
