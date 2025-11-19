d696236dbc8cca63eed5f7b689f5d479ab0a4460
Mario Nizic <marionizic@msn.com>
2025-11-19 12:56:46 +0100

    Added changes to ui_tasks.py, and test_smoke.py as per requested in PR review

--- tasks/ui_tasks.py
+++ tasks/ui_tasks.py
@@ -1,57 +1,59 @@
 from playwright.sync_api import Page
 
-def navigate_to_product_page(page: Page):
-    page.goto("/")
-    page.get_by_role("heading", name="Thor Hammer").click()
+class Tasks:
 
-def add_product_to_cart(page: Page):
-    page.get_by_role("button", name="Add to cart").click()
-    page.locator("[data-test='nav-cart']").click()
+    def navigate_to_product_page(page: Page):
+        page.goto("/")
+        page.get_by_role("heading", name="Thor Hammer").click()
 
-def navigate_to_registration(page: Page):
-    page.get_by_role("button", name="Proceed to checkout").click()
-    page.get_by_role("link", name="Register your account").click()
+    def add_product_to_cart(page: Page):
+        page.get_by_role("button", name="Add to cart").click()
+        page.locator("[data-test='nav-cart']").click()
 
-def complete_registration(page:Page, first_name, last_name, date_of_birth, street, postal_code, city, state, country, phone, email, password):
-    page.get_by_label("First name").fill(first_name)
-    page.get_by_label("Last name").fill(last_name)
-    page.get_by_label("Date of birth").fill(date_of_birth)
-    page.get_by_label("Street").fill(street)
-    page.get_by_label("Postal code").fill(postal_code)
-    page.get_by_label("City").fill(city)
-    page.get_by_label("State").fill(state)
-    page.get_by_label("Country").select_option(country)
-    page.get_by_label("Phone").fill(phone)
-    page.get_by_label("Email").fill(email)
-    page.get_by_label("Password").fill(password)
-    page.get_by_role("button", name="Register").click()
+    def navigate_to_registration(page: Page):
+        page.get_by_role("button", name="Proceed to checkout").click()
+        page.get_by_role("link", name="Register your account").click()
 
-def complete_login(page:Page, email, password):
-    page.get_by_label("Email address *").fill(email)
-    page.get_by_label("Password *").fill(password)
-    page.locator("[data-test='login-submit']").click()
+    def complete_registration(page:Page, first_name, last_name, date_of_birth, street, postal_code, city, state, country, phone, email, password):
+        page.get_by_label("First name").fill(first_name)
+        page.get_by_label("Last name").fill(last_name)
+        page.get_by_label("Date of birth").fill(date_of_birth)
+        page.get_by_label("Street").fill(street)
+        page.get_by_label("Postal code").fill(postal_code)
+        page.get_by_label("City").fill(city)
+        page.get_by_label("State").fill(state)
+        page.get_by_label("Country").select_option(country)
+        page.get_by_label("Phone").fill(phone)
+        page.get_by_label("Email").fill(email)
+        page.get_by_label("Password").fill(password)
+        page.get_by_role("button", name="Register").click()
 
-def navigate_to_cart_login(page: Page):
-    page.goto("/checkout")
-    page.get_by_role("button", name="Proceed to checkout").click()
+    def complete_login(page:Page, email, password):
+        page.get_by_label("Email address *").fill(email)
+        page.get_by_label("Password *").fill(password)
+        page.locator("[data-test='login-submit']").click()
 
-def complete_cart_login(page: Page, email, password):
-    page.locator("#email").fill(email)
-    page.locator("#password").fill(password)
-    page.locator("[data-test='login-submit']").click()
+    def navigate_to_cart_login(page: Page):
+        page.goto("/checkout")
+        page.get_by_role("button", name="Proceed to checkout").click()
 
-def proceed_to_payment(page: Page):
-    page.get_by_role("button", name="Proceed to checkout").click()
-    page.get_by_role("button", name="Proceed to checkout").click()
+    def complete_cart_login(page: Page, email, password):
+        page.locator("#email").fill(email)
+        page.locator("#password").fill(password)
+        page.locator("[data-test='login-submit']").click()
 
-def complete_payment(page: Page, payment_method, credit_card_number, expiration_date, cvv, credit_card_holder_name):
-    page.get_by_label("Payment Method").select_option(payment_method)
-    page.get_by_label("Credit Card Number").fill(credit_card_number)
-    page.get_by_label("Expiration Date").fill(expiration_date)
-    page.get_by_label("CVV").fill(cvv)
-    page.get_by_label("Card Holder Name").fill(credit_card_holder_name)
-    page.get_by_role("button", name="Confirm").click()
+    def proceed_to_payment(page: Page):
+        page.get_by_role("button", name="Proceed to checkout").click()
+        page.get_by_role("button", name="Proceed to checkout").click()
 
-def check_invoice(page: Page):
-    page.locator("#menu").click()
-    page.locator("[data-test='nav-my-invoices']").click()
+    def complete_payment(page: Page, payment_method, credit_card_number, expiration_date, cvv, credit_card_holder_name):
+        page.get_by_label("Payment Method").select_option(payment_method)
+        page.get_by_label("Credit Card Number").fill(credit_card_number)
+        page.get_by_label("Expiration Date").fill(expiration_date)
+        page.get_by_label("CVV").fill(cvv)
+        page.get_by_label("Card Holder Name").fill(credit_card_holder_name)
+        page.get_by_role("button", name="Confirm").click()
+
+    def check_invoice(page: Page):
+        page.locator("#menu").click()
+        page.locator("[data-test='nav-my-invoices']").click()
--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -2,10 +2,9 @@ import pytest
 import allure
 from playwright.sync_api import Page
 
-from tasks.ui_tasks import navigate_to_product_page, add_product_to_cart, navigate_to_registration, \
-    complete_registration, complete_login, navigate_to_cart_login, complete_cart_login, proceed_to_payment, \
-    complete_payment, check_invoice
+from tasks.ui_tasks import Tasks
 from utilities.user_data import USERS
+
 
 @allure.title("Smoke Test Demo Application")
 @allure.description("This is a smoke test for Demo Web Application, which tests basic features like add to cart, register, login, payment method selection, invoice validation.")
@@ -18,22 +17,22 @@ from utilities.user_data import USERS
 @pytest.mark.smoke
 def test_smoke(page: Page):
 
-    navigate_to_product_page(page)
+    Tasks.navigate_to_product_page(page)
 
-    add_product_to_cart(page)
+    Tasks.add_product_to_cart(page)
 
-    navigate_to_registration(page)
+    Tasks.navigate_to_registration(page)
 
-    complete_registration(page,USERS.user_credentials.first_name,USERS.user_credentials.last_name,USERS.user_credentials.date_of_birth,USERS.user_credentials.street, USERS.user_credentials.postal_code, USERS.user_credentials.city, USERS.user_credentials.state, USERS.user_credentials.country, USERS.user_credentials.phone, USERS.user_credentials.email , USERS.user_credentials.password)
+    Tasks.complete_registration(page,USERS.user_credentials.first_name,USERS.user_credentials.last_name,USERS.user_credentials.date_of_birth,USERS.user_credentials.street, USERS.user_credentials.postal_code, USERS.user_credentials.city, USERS.user_credentials.state, USERS.user_credentials.country, USERS.user_credentials.phone, USERS.user_credentials.email , USERS.user_credentials.password)
 
-    complete_login(page,USERS.user_credentials.email,USERS.user_credentials.password)
+    Tasks.complete_login(page,USERS.user_credentials.email,USERS.user_credentials.password)
 
-    navigate_to_cart_login(page)
+    Tasks.navigate_to_cart_login(page)
 
-    complete_cart_login(page, USERS.user_credentials.email, USERS.user_credentials.password)
+    Tasks.complete_cart_login(page, USERS.user_credentials.email, USERS.user_credentials.password)
 
-    proceed_to_payment(page)
+    Tasks.proceed_to_payment(page)
 
-    complete_payment(page, USERS.user_credentials.payment_method, USERS.user_credentials.credit_card_number, USERS.user_credentials.expiration_date, USERS.user_credentials.cvv, USERS.user_credentials.card_holder_name)
+    Tasks.complete_payment(page, USERS.user_credentials.payment_method, USERS.user_credentials.credit_card_number, USERS.user_credentials.expiration_date, USERS.user_credentials.cvv, USERS.user_credentials.card_holder_name)
 
-    check_invoice(page)
+    Tasks.check_invoice(page)
